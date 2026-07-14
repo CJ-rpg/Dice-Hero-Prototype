623af9ed8f089392d8658f2f1522fc979a174d17
CJ VanDenEng <cj.vandeneng@gmail.com>
2026-07-14 15:06:47 -0500

    added turn changes

--- game.py
+++ game.py
@@ -10,26 +10,76 @@ class Game:
         self.hero = Barbarian()
         self.rolls = []
         self.valid_abilities = []
+        self.opponent = Barbarian()
+        self.active_player = None
 
     def start(self):
-        self.hero.show_abilities()
+        self.decide_first_player()
+
+        while self.hero.health > 0 and self.opponent.health > 0:
+            self.attack_phase()
+            self.player_turn()
+
+        if self.hero.health <= 0:
+            print("You lose!")
+        else:
+            print("You win!")
+
+    def player_turn(self):
+        if self.active_player == self.hero:
+            self.active_player = self.opponent
+        else:
+            self.active_player = self.hero
+
+    def decide_first_player(self):
+        while self.active_player is None:
+            hero_roll = r.choice(self.hero._dice)
+            opponent_roll = r.choice(self.opponent._dice)
+
+            print(f"You rolled {hero_roll['num']}")
+            print(f"Opponent rolled {opponent_roll['num']}")
+
+            if hero_roll["num"] > opponent_roll["num"]:
+                self.active_player = self.hero
+                print("You go First")
+            elif opponent_roll["num"] > hero_roll["num"]:
+                self.active_player = self.opponent
+                print("Opponent Goes First")
+            else:
+                print("Tie! Rolling again...")
+            
+    def get_opponent(self):
+        if self.active_player == self.hero:
+            return self.opponent
+        return self.hero
+    
+    def show_rolls(self):
+            for die in self.rolls:
+                print(f"{die['num']} - {die['symbol']}")
+
+    def attack_phase(self):
+        print(f"{self.active_player.name}'s Turn")
+        print(f"Heros Health: {self.hero.health}")
+        print(f"Opponents Health: {self.opponent.health}")
+
+        self.active_player.show_abilities()
         attempts = 0
 
-        self.rolls = [r.choice(self.hero._dice) for _ in range(5)]
+        self.rolls = [r.choice(self.active_player._dice) for _ in range(5)]
         self.show_rolls()
 
         while attempts < 2:
-            self.valid_abilities, symbol_counts = check_abilities(self.hero, self.rolls)
+            self.valid_abilities, symbol_counts = check_abilities(self.active_player, self.rolls)
 
             for i in range(len(self.rolls)):
                 reroll = input(f"Would you like to reroll {self.rolls[i]}? (y/n) ")
 
                 if reroll.lower() == "y":
-                    self.rolls[i] = r.choice(self.hero._dice)
+                    self.rolls[i] = r.choice(self.active_player._dice)
 
             self.show_rolls()
 
-            self.valid_abilities, symbol_counts = check_abilities(self.hero, self.rolls)
+            self.valid_abilities, symbol_counts = check_abilities(self.active_player, self.rolls)
 
             attempts += 1
             if attempts < 2:
@@ -39,27 +89,24 @@ class Game:
                     break
 
 
-
+        if self.valid_abilities == []:
+            print("Sorry no Valid Abilities")
+            return
         choice = int(input("Choose an ability: "))
         selected = self.valid_abilities[choice - 1]
 
         #self._abilities["smack"]["function"](3)
         if selected == "smack":
-            self.hero.smack(symbol_counts["sword"])
+            self.active_player.smack(symbol_counts["sword"])
         elif selected == "fortitude":
-            self.hero.fortitude(symbol_counts["heart"])
+            self.active_player.fortitude(symbol_counts["heart"])
         else:
-            getattr(self.hero, selected)()
+            getattr(self.active_player, selected)()
 
-    def player_turn(self):
-        pass
+        defender = self.get_opponent()
+        defender.take_damage(10)
 
-    def attack_phase(self):
-        pass
-
-    def show_rolls(self):
-        for die in self.rolls:
-            print(f"{die['num']} - {die['symbol']}")
+    
 
 def check_abilities(hero, rolls):
     nums = [die["num"] for die in rolls]   
